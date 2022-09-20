d60f740ed489768c05f8f8ba0c98d56dc019f1b0
Yur4uc <75786848+Yur4uc@users.noreply.github.com>
2022-09-20 23:54:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let vsiTitle = document.getElementsByClassName("item-grid-card__title")
-
-let vsiTitleArray = Array.from(vsiTitle)
-
-var mapT = vsiTitleArray.map(t => {return t.innerText})
-
-console.log(mapT)
+let vsiTitle = document.getElementsByClassName("item-grid-card");
+let res = Array.from(vsiTitle).map(elem => {
+    return  {
+        name: elem.querySelector('.item-grid-card__title').innerText,
+        src: elem.querySelector('[data-controller="audio-player"]').getAttribute('data-audio-player-waveform-url-value')
+    }
+})
+console.log(res)
